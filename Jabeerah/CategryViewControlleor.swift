3cc119f301a75386731a9a47aa2bb3f01b3e6213
JabeerahiOS <alaa@macbook-pro-alkhas-b-mohammed.local>
2016-08-21 15:38:58 +0300

    alaa

---
+++ Jabeerah/CategryViewControlleor.swift
@@ -0,0 +1,121 @@
+//
+//  CategoryViewController.swift
+//  Jabeerah
+//
+//  Created by alaa on 11/18/1437 AH.
+//  Copyright © 1437 Jabeerah. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CategryViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var imagePicker : UIImagePickerController = UIImagePickerController()
+    
+    
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let theInfo:NSDictionary = info as NSDictionary
+        let img:UIImage = theInfo.objectForKey(UIImagePickerControllerOriginalImage) as! UIImage
+        ImageView.image = img
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func AddPictureBtnAction(sender: UIButton) {
+   
+        
+        
+        // addPictureBtnAtion.enabled = false
+        let alertController : UIAlertController = UIAlertController(title: "أضف جهازًا", message: "التقط صورة من الكاميرا أو اخترالألبوم", preferredStyle: .ActionSheet)
+        
+        let cameraAction : UIAlertAction = UIAlertAction(title: "صورة من الكاميرا", style: .Default, handler: {(cameraAction) in
+            print("camera Selected...")
+            
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) == true {
+                
+                self.imagePicker.sourceType = .Camera
+                self.present()
+                
+            }else{
+                self.presentViewController(self.showAlert("عذرًا", Message: "الكاميرا ليست متاحة في هذا الجهاز أو تم منع الوصول لها!"), animated: true, completion: nil)
+                
+            }
+            
+        })
+        
+        let libraryAction : UIAlertAction = UIAlertAction(title: "صورة من الألبوم", style: .Default, handler: {(libraryAction) in
+            
+            print("Photo library selected....")
+            
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) == true {
+                
+                self.imagePicker.sourceType = .PhotoLibrary
+                self.present()
+                
+            }else{
+                
+                self.presentViewController(self.showAlert("عذرًا", Message: "ألبوم الصور ليس متاحًا في هذا الجهاز أو تم منع الوصول له!"), animated: true, completion: nil)
+            }
+        })
+        
+        let cancelAction : UIAlertAction = UIAlertAction(title: "إلغاء", style: .Cancel , handler: {(cancelActn) in
+            print("Cancel action was pressed")
+        })
+        
+        alertController.addAction(cameraAction)
+        
+        alertController.addAction(libraryAction)
+        
+        alertController.addAction(cancelAction)
+        
+        alertController.popoverPresentationController?.sourceView = view
+        alertController.popoverPresentationController?.sourceRect = view.frame
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    func present(){
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    /*   func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+     print("info of the pic reached :\(info) ")
+     self.imagePicker.dismissViewControllerAnimated(true, completion: nil)
+     
+     } */
+    
+    //Show Alert
+    func showAlert(Title : String!, Message : String!)  -> UIAlertController {
+        
+        let alertController : UIAlertController = UIAlertController(title: Title, message: Message, preferredStyle: .Alert)
+        let okAction : UIAlertAction = UIAlertAction(title: "Ok", style: .Default) { (alert) in
+            print("User pressed ok function")
+            
+        }
+        alertController.addAction(okAction)
+        alertController.popoverPresentationController?.sourceView = view
+        alertController.popoverPresentationController?.sourceRect = view.frame
+        return alertController
+    }
+    
+    
+    
+}
+    
