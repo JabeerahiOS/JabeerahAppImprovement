08e097834ef76ca7831d8a5941715eeb86437e86
Mariah KH <Mariah@mariahs-macbook-pro.local>
2016-08-29 16:46:40 +0300

    Edit Password, Done

--- Jabeerah/EditPasswordViewController.swift
+++ Jabeerah/EditPasswordViewController.swift
@@ -29,9 +29,58 @@ class EditPasswordViewController: UIViewController {
     }
     
     @IBAction func EditPassword(sender: AnyObject) {
+        if NewPassword.text == "" || ReNewPassword.text == "" {
+            let alert = UIAlertController(title: "عذرًا", message:"يجب عليك ملىء كل الحقول المطلوبة", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "نعم", style: .Default) { _ in })
+            self.presentViewController(alert, animated: true){}
+
+        } else {
+        
+            if NewPassword.text != ReNewPassword.text {
+                
+                let alert = UIAlertController(title: "عذرًا", message:"كلمتي المرور غير متطابقتين", preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "نعم", style: .Default) { _ in })
+                self.presentViewController(alert, animated: true){}
+            
+            } else {
+            
+             FIRAuth.auth()!.currentUser!.updatePassword(NewPassword.text!, completion: { (error) in
+                
+                if error != nil {
+                    
+                    switch FIRAuthErrorCode(rawValue:error!.code)! {
+                    case .ErrorCodeNetworkError:
+                        let alert = UIAlertController(title: "عذرًا", message:"خطأ في الشبكة", preferredStyle: .Alert)
+                        alert.addAction(UIAlertAction(title: "نعم", style: .Default) { _ in })
+                        self.presentViewController(alert, animated: true){}
+                        
+                    case .ErrorCodeWeakPassword:
+                        let alert = UIAlertController(title: "عذرًا", message:"كلمة المرور ضعيفة", preferredStyle: .Alert)
+                        alert.addAction(UIAlertAction(title: "نعم", style: .Default) { _ in })
+                        self.presentViewController(alert, animated: true){}
+                        
+                    default:
+                        let alert = UIAlertController(title: "عذرًا", message:"خطأ غير معروف", preferredStyle: .Alert)
+                        alert.addAction(UIAlertAction(title: "نعم", style: .Default) { _ in })
+                        self.presentViewController(alert, animated: true){}
+                        
+                    }
+                
+                } else {
+                print("Password Updated Successfully")
+                self.performSegueWithIdentifier("EditPassword", sender: nil)
+                
+                }
+                
+             })
+            
+            
+            
+            }
+        }
         
      
-              //self.performSegueWithIdentifier("EditPassword", sender: nil)
+        
     }
 
     /*
