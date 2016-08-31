bb663b339bfd736f239e7c6306cee6a5d6ea036a
JabeerahiOS <JabeerahiOS@gmail.com>
2016-08-31 15:08:37 +0300

    H1

--- Jabeerah/HomePage1.swift
+++ Jabeerah/HomePage1.swift
@@ -8,31 +8,36 @@
 
 import UIKit
 
-class HomePage1: UIViewController , UITableViewDataSource {
-    
-    let Category = ["أخرى","أسرة طبية","أدوات دورات المياة","أجهزة قياس","عكازات","عربات متحركة"]
+class HomePage1: UIViewController , UITableViewDataSource , UITableViewDelegate {
     
     @IBOutlet weak var categoryTable: UITableView!
-   
+
+    var Category = ["أخرى","أسرة طبية","أدوات دورات المياة","أجهزة قياس","عكازات","عربات متحركة"]
+    var images = [UIImage(named: "icon-181919"),UIImage(named: "icon-139466"),UIImage(named: "icon-181919"),UIImage(named: "icon-10717"),UIImage(named: "icon-198070"),UIImage(named: "icon-45650"),UIImage(named: "icon-26589")]
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+
+    
     public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
           return Category.count
     }
+    
+    
     public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        let myCell : UITableViewCell = categoryTable.dequeueReusableCellWithIdentifier("Category1", forIndexPath: indexPath)as!UITableViewCell
-    
-        myCell.textLabel?.text = Category[indexPath.row]
-        mycell.imageView?.image = UIImage(named:Category[indexPath.row] )
-        return myCell
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)as!customCell
+       cell.photo.images = images[indexPath.row]
+        cell.name.text = Category[indexPath.row]
+        return cell
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        categoryTable.dataSource = self
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-}
+  }
