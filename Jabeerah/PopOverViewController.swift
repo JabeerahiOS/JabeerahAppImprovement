dbc3915a996b2f536919cad6b7328606aff0b984
Mariah KH <Mariah@mariahs-macbook-pro.local>
2016-09-09 01:48:26 +0300

    The reason why I did not want to push the changes here! Because I felt that 38769354867987698379687254111 of people are monitoring the progress! And that's really annoying me! Once we finished it perfectly! We will pushed itمرة واحدة. !

--- Jabeerah/PopOverViewController.swift
+++ Jabeerah/PopOverViewController.swift
@@ -20,6 +20,7 @@ class PopOverViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     
     
+    
     @IBOutlet weak var ScrollView: UIScrollView!
     @IBOutlet weak var ImageView: UIImageView!
     @IBOutlet weak var DeviceName: UITextField!
